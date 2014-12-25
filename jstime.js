7e190bb3d256905d629cf57fef542f91faf7244e
tozcentral <tozcentral@googlemail.com>
2014-12-25 13:29:52 +0000

    Added some redefined date formats like RFC 1123

--- jstime.js
+++ jstime.js
@@ -253,7 +253,22 @@ function scanPage ( )
 window.JSTime = {
 	format: formatTime,
 	updateNode: updateNode,
-	scanPage: scanPage
+	scanPage: scanPage,
+	
+	ATOM: 		'Y-m-d\TH:i:sP',
+    COOKIE: 	'l, d-M-Y H:i:s T',
+    HTTP: 		'D, d M y H:i:s O',
+    RSS: 		'D, d M Y H:i:s O',
+    W3C: 		'Y-m-d\\TH:i:sP',
+	
+    ISO8601: 	'Y-m-d\\TH:i:sO',
+	
+    RFC822: 	'D, d M y H:i:s O',
+    RFC850: 	'l, d-M-y H:i:s T',
+    RFC1036: 	'D, d M y H:i:s O',
+    RFC1123: 	'D, d M Y H:i:s O',
+    RFC2822: 	'D, d M Y H:i:s O',
+    RFC3339: 	'Y-m-d\\TH:i:sP'
 };
 
 scanPage ( )
